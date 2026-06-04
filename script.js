7181b07e54ed92062945942080dd00ef0dcdf514
kessyarafaella097-debug <kessyarafaella097@gmail.com>
2026-06-04 11:47:27 -0300

    Add agendar function to schedule appointments

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function agendar(){
 
+let nome = document.getElementById("nome").value;
+let telefone = document.getElementById("telefone").value;
+let servico = document.getElementById("servico").value;
+let data = document.getElementById("data").value;
+let horario = document.getElementById("horario").value;
+
+let mensagem =
+`Olá, Kessya!
+
+Gostaria de agendar um horário.
+
+Nome: ${nome}
+Telefone: ${telefone}
+Procedimento: ${servico}
+Data: ${data}
+Horário: ${horario}`;
+
+let numero = "5583999999999";
+
+window.open(
+`https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`
+);
+
+}
